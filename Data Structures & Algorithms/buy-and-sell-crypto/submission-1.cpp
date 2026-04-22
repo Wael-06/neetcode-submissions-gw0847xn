ece7558c3df8c71258aeaa4bb8d02aae1ea5c007
Wael-06 <youssefwael.work@gmail.com>
2026-04-22 21:42:43 +0200

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy=0,maxprofit=0;
+        for(int i=1; i<prices.size();i++){
+            if(prices[i]<prices[buy]){
+                buy=i;
+            }
+            else{
+                maxprofit=max(maxprofit,prices[i]-prices[buy]);
+            }
+        }
+    return maxprofit;
+    }
+};
